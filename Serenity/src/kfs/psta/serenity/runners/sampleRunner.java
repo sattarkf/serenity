635f7f4aa0db4a32325ea428d85c69335a4a6bd4
sattarkf <kfsattar@gmail.com>
2016-03-07 21:39:25 -0700

    Initial Checking of Serenity sample test code.

---
+++ Serenity/src/kfs/psta/serenity/runners/sampleRunner.java
@@ -0,0 +1,14 @@
+package kfs.psta.serenity.runners;
+
+public class sampleRunner {
+
+	public static void main(String[] args) {
+		displayGreeting();
+
+	}
+
+	private static void displayGreeting() {
+		System.out.println("HELLO SERENITY TEAMs");
+	}
+
+}
